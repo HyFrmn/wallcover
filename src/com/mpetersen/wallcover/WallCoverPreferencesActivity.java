c2715cece50588d180a95a2b0b2863e235f8b273
Marshall Petersen <marshall.petersen@gmail.com>
2012-11-06 22:06:01 -0800

    Added file picker dialog.

--- src/com/mpetersen/wallcover/WallCoverPreferencesActivity.java
+++ src/com/mpetersen/wallcover/WallCoverPreferencesActivity.java
@@ -21,6 +21,10 @@ public class WallCoverPreferencesActivity extends PreferenceActivity implements
 		SharedPreferences.OnSharedPreferenceChangeListener {
 
 	public static final String SHARED_PREFS_NAME = "preferences";
+	
+	private static final int FILE_SELECT_CODE = 1;
+	private static final int IMAGE_SELECT_CODE = 0;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -46,16 +50,45 @@ public class WallCoverPreferencesActivity extends PreferenceActivity implements
 						Intent photoPickerIntent = new Intent(
 								Intent.ACTION_PICK);
 						photoPickerIntent.setType("image/*");
-						startActivityForResult(photoPickerIntent, 1);
+						startActivityForResult(photoPickerIntent, IMAGE_SELECT_CODE);
 						return true;
 					}
 				});
+		
+		prefs.findPreference("image_filepath")
+		.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+				
+				Display display = getWindowManager()
+						.getDefaultDisplay();
+				int width = display.getWidth();
+				int height = display.getHeight();
+				Toast.makeText(getBaseContext(),
+						"Select Image - " + (width) + " x " + height,
+						Toast.LENGTH_LONG).show();
+				
+				Intent intent = new Intent(Intent.ACTION_GET_CONTENT); 
+		        intent.setType("*/*"); 
+		        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+		        try {
+		            startActivityForResult(
+		                    Intent.createChooser(intent, "Select a File to Upload"),
+		                    FILE_SELECT_CODE);
+		        } catch (android.content.ActivityNotFoundException ex) {
+		            // Potentially direct the user to the Market with a Dialog
+		            Toast.makeText(getBaseContext(), "Please install a File Manager.", 
+		                    Toast.LENGTH_SHORT).show();
+		        }
+				return true;
+			}
+		});
 	}
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == 1) {
+		if (requestCode == IMAGE_SELECT_CODE) {
 			if (resultCode == Activity.RESULT_OK) {
 				Uri selectedImage = data.getData();
 				String RealPath;
@@ -63,6 +96,22 @@ public class WallCoverPreferencesActivity extends PreferenceActivity implements
 						SHARED_PREFS_NAME, Context.MODE_PRIVATE);
 				SharedPreferences.Editor editor = customSharedPreference.edit();
 				RealPath = getRealPathFromURI(selectedImage);
+				editor.putString("image_filepath", RealPath);
+				editor.commit();
+				getPreferenceManager().findPreference("image_filepath")
+						.setSummary(RealPath);
+
+			}
+		}
+		
+		if (requestCode == FILE_SELECT_CODE) {
+			if (resultCode == Activity.RESULT_OK) {
+				Uri selectedImage = data.getData();
+				String RealPath;
+				SharedPreferences customSharedPreference = getSharedPreferences(
+						SHARED_PREFS_NAME, Context.MODE_PRIVATE);
+				SharedPreferences.Editor editor = customSharedPreference.edit();
+				RealPath = selectedImage.getEncodedPath();
 				editor.putString("image_filepath", RealPath);
 				editor.commit();
 				getPreferenceManager().findPreference("image_filepath")
